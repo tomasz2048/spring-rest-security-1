83317c46972e2c7d818acd5c9e43671e26f33012
Tomasz Zdebski <tomasz2048@gmail.com>
2023-07-30 09:20:02 -0500

    .

--- src/main/java/com/example/springrestsecurity1/SecurityConfig.java
+++ src/main/java/com/example/springrestsecurity1/SecurityConfig.java
@@ -2,13 +2,13 @@ package com.example.springrestsecurity1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
